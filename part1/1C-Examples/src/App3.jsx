553ac606082d5e6ab5c574da51b4a13d68393e08
Justinr922 <justinr922@gmail.com>
2024-12-06 15:35:56 -0500

    Separate 1c and 1d

---
+++ part1/1C-Examples/src/App3.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+
+const App3 = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
+
+  return (
+    <div>
+      <Display counter={counter} />{" "}
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+    </div>
+  );
+};
+
+export default App3;
