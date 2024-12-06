c2f7c058c5271081f1fcdb13d0e63364b7363436
Justinr922 <justinr922@gmail.com>
2024-12-06 15:08:06 -0500

    Add component helper functions

--- part1/part1Examples/src/App.jsx
+++ part1/part1Examples/src/App.jsx
@@ -2,19 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-const Hello = (props) => {
+const Hello = ({age, name}) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - age;
+  };
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p>So you were probably born in {bornYear()} </p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const name = "Peter";
+  const age = 10;
 
   return (
     <div>
@@ -22,7 +28,7 @@ const App = () => {
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
     </div>
-  )
+  );
 };
 
 export default App;
