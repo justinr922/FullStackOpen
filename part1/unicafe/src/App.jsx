23ba49662ea7f012826bb6f36756af3e3b913d6f
Justinr922 <justinr922@gmail.com>
2024-12-07 18:47:47 -0500

    Conditionally render statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,27 +7,44 @@ const FeedbackButton = (props) => {
 
 const Result = (props) => {
   const { p_text, p_result, p_suffix } = props;
-  console.log(p_result)
+  console.log(p_result);
   return (
     <p>
-      {p_text} {p_result}{p_suffix}
+      {p_text} {p_result}
+      {p_suffix}
     </p>
   );
 };
 
 const Statistics = (props) => {
-  const {p_good, p_neutral, p_bad, p_total, p_average, p_percentPositive} = props;
+  const { p_good, p_neutral, p_bad, p_total, p_average, p_percentPositive } =
+    props;
+
+  if (p_total === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
   return (
-  <div>
+    <div>
       <h1>Statistics</h1>
       <Result p_result={p_good} p_text="Good" />
       <Result p_result={p_neutral} p_text="Neutral" />
       <Result p_result={p_bad} p_text="Bad" />
-      <Result p_result={p_total} p_text="All"/>
-      <Result p_result={p_average || 0} p_text="Average"/>
-      <Result p_result={p_percentPositive.toFixed(2)} p_text="Positive" p_suffix='%'/>
-  </div>
-)}
+      <Result p_result={p_total} p_text="All" />
+      <Result p_result={p_average || 0} p_text="Average" />
+      <Result
+        p_result={p_percentPositive.toFixed(2)}
+        p_text="Positive"
+        p_suffix="%"
+      />
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -40,7 +57,7 @@ const App = () => {
   const handleBadClick = () => setBad(bad + 1);
   const total = good + bad + neutral;
   const average = (good - bad) / total;
-  const percentPositive = ((good / total) || 0) * 100.0
+  const percentPositive = (good / total || 0) * 100.0;
 
   return (
     <div>
@@ -48,7 +65,14 @@ const App = () => {
       <FeedbackButton p_handleClick={handleGoodClick} p_text="Good" />
       <FeedbackButton p_handleClick={handleNeutralClick} p_text="Neutral" />
       <FeedbackButton p_handleClick={handleBadClick} p_text="Bad" />
-      <Statistics p_good={good} p_neutral={neutral} p_bad={bad} p_total={total} p_average={average} p_percentPositive={percentPositive}/>
+      <Statistics
+        p_good={good}
+        p_neutral={neutral}
+        p_bad={bad}
+        p_total={total}
+        p_average={average}
+        p_percentPositive={percentPositive}
+      />
     </div>
   );
 };
