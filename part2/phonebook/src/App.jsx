23a9ad253f36dc9116e837bf2b7ace4a057190a0
Justinr922 <justinr922@gmail.com>
2024-12-23 12:48:32 -0600

    Add name search filter

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -24,15 +24,22 @@ const NewEntryForm = (props) => {
   )
 }
 
+
+
 const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
       number: '123-5555'
+    },
+    {
+      name:'Jartha',
+      number:'456-5555'
     }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     // console.log(newName)
@@ -60,13 +67,23 @@ const App = () => {
     }
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <input value={filter} onChange={handleFilterChange}/>
+      </form>
+      <h2>Add a new entry</h2>
       <NewEntryForm onNameChange={handleNameChange} onNumberChange={handleNumberChange} onSubmitForm={addPerson} newName={newName} newNumber={newNumber} />
       <h2>Numbers</h2>
       <ul>
-        {persons.map(x => <li key={x.name}>{x.name} {x.number}</li>)}
+        {persons
+        .filter(x => x.name.toLowerCase().includes(filter.toLowerCase()))
+        .map(x => <li key={x.name}>{x.name} {x.number}</li>)}
       </ul>
     </div>
   )
