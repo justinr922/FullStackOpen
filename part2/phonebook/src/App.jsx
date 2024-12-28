7d48f1f6150fec129cb13570cb7075cfe8012680
Justinr922 <justinr922@gmail.com>
2024-12-28 14:06:44 -0600

    Add person service

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios  from 'axios'
+import personService from './services/persons'
 
 
 const NewEntryForm = (props) => {
@@ -67,8 +68,8 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then((response) => {
         setPersons(response.data)
       })
